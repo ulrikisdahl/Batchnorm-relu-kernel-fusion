932a9a991e5b6d2af0928205b4f8484be62729f9
ulrikisdahl <uisdahl@gmail.com>
2024-06-13 20:54:20 +0200

    ci: add new porject setup

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-setup(name='binder',
-      ext_modules=[cpp_extension.CppExtension('binder', ['binder.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+setup(
+    name='binder',
+    ext_modules=[
+        CUDAExtension(
+            name='binder',
+            sources=['src/binder.cpp', 'src/fused_ops_kernel.cu'],
+            include_dirs=['/ulrik/home/libtorch/include', '/ulrik/home/libtorch/include/torch/csrc/api/include', "/usr/local/cuda/include"],
+            library_dirs=['/ulrik/home/libtorch/lib'],
+            libraries=['c10', 'torch', 'torch_cpu', 'torch_cuda']
+        )
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
+)
+
+#python3 setup.py build_ext --inplace
