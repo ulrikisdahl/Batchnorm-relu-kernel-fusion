68857255c0dd961e68d92e38c709a63ce2712490
ulrikisdahl <uisdahl@gmail.com>
2024-06-15 14:18:44 +0200

    feat: Add torch.autograd.Function implementation

--- model.py
+++ model.py
@@ -1,35 +1,54 @@
 import torch
+import torch.nn as nn
 from torch.autograd import Function
 from binder import bn_relu_forward, bn_relu_backward
 
-#TODO: Create lambda and beta parameters 
+device="cuda"
 
-class BatchNormRelu(Function):
+class BatchNormReluFunction(Function):
     @staticmethod
-    def forward(ctx, input_tensor):
+    def forward(ctx, input_tensor, lambdas, betas):
         """
         Args:
             ctx: context object
             inpu_tensor: input tensor to the batchnorm operation
+            lambdas: scaling parameter for the batchnorm operation
+            betas: shift parameter for the batchnorm operation
         """
 
-        ctx.save_for_backward(input_tensor) #stash information for backward pass
-        output = bn_relu_forward(input_tensor)
+        output, means, stddevs = bn_relu_forward(input_tensor, lambdas, betas)
+        ctx.save_for_backward(input_tensor, lambdas, betas, means, stddevs) #stash information for backward pass
         return output
     
     @staticmethod
-    def backward(ctx, grad_output): #must have same numer of arguments as outputs in forward
+    def backward(ctx, output_grad): #must have same numer of arguments as outputs in forward
         """
         Args:
             ctx: context object
             grad_output: gradient of the output from the forward operation
         """
+        input_tensor, lambdas, betas, means, stddevs = ctx.saved_tensors
+        input_grad = torch.empty_like(output_grad, device=device)
+        lambdas_grad = torch.zeros_like(lambdas, device=device)
+        betas_grad = torch.zeros_like(betas, device=device) 
+        input_grad, lambdas_grad, betas_grad = bn_relu_backward(
+            output_grad, input_tensor, input_grad, lambdas, betas, lambdas_grad, betas_grad, means, stddevs
+            )
+        return input_grad, lambdas_grad, betas_grad
+    
+class BatchNormRelu(nn.Module):
+    def __init__(self, channels, resolution):
+        super(BatchNormRelu, self).__init__()
+
+        self.lambdas = nn.Parameter(
+            torch.ones(channels, resolution, resolution, device=device)
+        )
+        self.betas = nn.Parameter(
+            torch.zeros(channels, resolution, resolution, device=device)
+        )
+    
+    def forward(self, inp):
+        return BatchNormReluFunction.apply(inp, self.lambdas, self.betas)
         
-        return 
-
-        
-
-
-
 
 
